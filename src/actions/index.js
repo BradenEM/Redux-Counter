4e2d96a75b44ec3866da51cba46bb58f24cb36eb
Braden Mason <bradenemason@gmail.com>
2019-06-20 16:50:56 -0500

    MVP Done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
+// import { type } from "os";
+
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
 
 // Our action creators will return
 // an action packet that our reducer will
@@ -9,10 +11,16 @@ export const DECREMENT = 'DECREMENT';
 // logic of updating the central Redux store. That
 // is left to the reducer(s).
 
-export const increment = () => {
-  // Fill in this function
+export const increment = count => {
+  return {
+    type: INCREMENT,
+    payload: count
+  };
 };
 
-export const decrement = () => {
-  // Fill in this function
+export const decrement = count => {
+  return {
+    type: DECREMENT,
+    payload: count
+  };
 };
